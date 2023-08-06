6ca33563bdbbbc985e437425a9990c89c6754bf5
izuutech <izutechsofficial@gmail.com>
2023-08-06 17:57:27 +0100

    hide link in order history

--- pages/account/order-history.js
+++ pages/account/order-history.js
@@ -55,35 +55,21 @@ function OrderHistory() {
                 />
               ))}
 
-            <nav className="float-end mt-3">
+            {/* <nav className="float-end mt-3">
               <ul className="pagination">
                 <li className="page-item">
                   <a className="page-link" href="#">
                     Prev
                   </a>
                 </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    1
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    2
-                  </a>
-                </li>
-                <li className="page-item">
-                  <a className="page-link" href="#">
-                    3
-                  </a>
-                </li>
+               
                 <li className="page-item">
                   <a className="page-link" href="#">
                     Next
                   </a>
                 </li>
               </ul>
-            </nav>
+            </nav> */}
           </div>
         </div>
       </div>
