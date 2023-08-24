e9a1e13560b60d639f246b719f14fb68884ab177
izuutech <izutechsofficial@gmail.com>
2023-08-24 21:29:52 +0100

    frontbase

--- utils/constants.utils.js
+++ utils/constants.utils.js
@@ -1,5 +1,4 @@
 export const baseUrll = "http://localhost:4000/";
 export const frontendbaseUrll = "http://localhost:3000/";
 export const baseUrl = "https://affiliate-eccomerce-project.vercel.app/";
-export const frontendbaseUrl =
-  "https://affiliate-ecomerce-gecbmvrzg-izuutech.vercel.app/";
+export const frontendbaseUrl = "https://affiliate-ecomerce.vercel.app/";
